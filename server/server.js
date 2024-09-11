4a32baaa82eb4050ce90de8a01c86cc480ec653e
Rijo Sebastian <rijobdk@gmail.com>
2024-09-11 10:53:06 +0530

    Add register and login controllers

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import indexRouter from "./routes/index.js";
+import connectDB from "./config/db.js";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.use("/api", indexRouter);
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log("Error in starting server", err);
+  }
+};
+
+startServer();
